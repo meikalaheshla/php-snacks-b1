d3082de53a21133a232813dbe186118216c8ff61
meikalaheshla <fausto.boshoff@gmail.com>
2022-08-30 16:25:32 +0200

    first commit

---
+++ snack1.php
@@ -0,0 +1,46 @@
+<!-- SNACK 1  -->
+
+<!-- Creo l'array contenente i risultati delle partite -->
+
+
+<?=
+$games = [
+    'home_team' => 'pippoteam',
+    'visiting_team' => 'plutoteam',
+    'score' => '28-45',
+],
+[
+    'home_team' => 'paperinooteam',
+    'visiting_team' => 'topolinoteam',
+    'score' => '28-45'
+],
+[
+    'home_team' => 'nonteam',
+    'visiting_team' => 'moltoteam',
+    'score' => '28-45'
+],
+[
+    'home_team' => 'pocoteam',
+    'visiting_team' => 'mancotroppoteam',
+    'score' => '28-45'
+];
+    var_dump ($games);
+    ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    </ul>
+    
+</body>
+</html>
