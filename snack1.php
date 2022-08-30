105048a7fe42f6e089ff5d5a9f340d6aa6f15d6e
meikalaheshla <fausto.boshoff@gmail.com>
2022-08-30 16:52:27 +0200

    snack 1

--- snack1.php
+++ snack1.php
@@ -3,28 +3,33 @@
 <!-- Creo l'array contenente i risultati delle partite -->
 
 
-<?=
+<?php
 $games = [
-    'home_team' => 'pippoteam',
-    'visiting_team' => 'plutoteam',
-    'score' => '28-45',
-],
-[
-    'home_team' => 'paperinooteam',
-    'visiting_team' => 'topolinoteam',
-    'score' => '28-45'
-],
-[
-    'home_team' => 'nonteam',
-    'visiting_team' => 'moltoteam',
-    'score' => '28-45'
-],
-[
-    'home_team' => 'pocoteam',
-    'visiting_team' => 'mancotroppoteam',
-    'score' => '28-45'
+    [
+
+        'home_team' => 'pippoteam',
+        'visiting_team' => 'plutoteam',
+        'score' => '28-45',
+    ],
+    [
+        'home_team' => 'paperinooteam',
+        'visiting_team' => 'topolinoteam',
+        'score' => '28-45'
+    ],
+    [
+        'home_team' => 'nonteam',
+        'visiting_team' => 'moltoteam',
+        'score' => '28-45'
+    ],
+    [
+        'home_team' => 'pocoteam',
+        'visiting_team' => 'mancotroppoteam',
+        'score' => '28-45'
+    ],
 ];
-    var_dump ($games);
+var_dump ($games);
+
+
     ?>
 
 
@@ -40,6 +45,13 @@ $games = [
 </head>
 <body>
     <ul>
+        <?php for($i = 0; $i< count($games); $i++):?>
+        <li>
+            <h1>Home Team: <?php echo $games[$i]['home_team'];?> </h1>
+            <h3>Visiting Team: <?php echo $games[$i]['visiting_team'];?></h3>
+            <span>Score: <?php echo $games[$i]['score'];?></span>
+        </li>
+        <?php endfor;?>
     </ul>
     
 </body>
