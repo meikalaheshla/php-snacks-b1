58d06d2654ab19ff72c37877642671272fe5b2c8
meikalaheshla <fausto.boshoff@gmail.com>
2022-08-31 02:31:32 +0200

    snack 3 printed on echo and html

--- snack3.php
+++ snack3.php
@@ -39,5 +39,77 @@ $posts = [
         ]
     ],
 ];
-var_dump ($posts);
+ /* 
+ foreach($posts as $key => $value){
+     echo $key;
+    foreach($value as $sub_key => $sub_val){
+         if (is_array($sub_val)) {
+            echo $sub_key . " : \n";
+            foreach ($sub_val as $k => $v) {
+                 echo  "\t"  .$k . " = " . $v  . "\n" ;
+            }
+        } else {
+             echo $sub_key . " = " . $sub_val . "\n";
+        }
+    }
+    
+ }
+ 
+
+  */
+ 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php
+        foreach($posts as $key => $value){
+            ?>
+            <li>
+                <h1><?= $key;?> </h1>
+            
+           <?php foreach($value as $sub_key => $sub_val){
+               // if (is_array($sub_val)) {
+               //     echo $sub_key . " : \n";
+                   foreach ($sub_val as $k => $v) {
+                      
+                   
+               // } else {
+               //     echo $sub_key . " = " . $sub_val . "\n";
+               
+           
+           
+       // }      
+        ?>
+        
+            <h2> <?= $k . " "."="." ". $v;?> </h2>
+            
+
+        </li>
+        <?php } ?>
+        <?php } ?>
+        <?php } ?>
+    </ul>
+
+    
+    
+</body>
+</html>
